38e34a89853a2052506988337c2e143d033eab51
Shofi Ullah <ullah.shofi.2023@gmail.com>
2023-11-18 23:12:20 -0500

    second commit

---
+++ src/main/java/com/generic/Cart.java
@@ -0,0 +1,10 @@
+package com.generic;
+
+public class Cart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,11 @@
+package com.generic;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You're Logged In");
+
+	}
+
+}
